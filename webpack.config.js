b68b986ab77d80840bbd0e60560a6d043a9f9f21
jbaptista <jbaptista@gobots.com.br>
2024-09-30 02:20:38 -0300

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require("./compilation.config.js");
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://secompp-cart.onrender.com/",
+    publicPath: "auto",
   },
 
   resolve: {
